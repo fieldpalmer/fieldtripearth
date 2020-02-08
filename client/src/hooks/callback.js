3f126937ecf373455dddde4b45253f6d35d820d0
fieldpalmer <gfieldpalmer@gmail.com>
2020-02-08 00:25:45 -0500

    header links and state context access

--- client/src/hooks/callback.js
+++ client/src/hooks/callback.js
@@ -1,1 +1,6 @@
-// used as the component that Auth0 will redirect to after user authenticats
+// used as the component that Auth0 will redirect to after user authenticates
+import React from "react";
+
+const Callback = props => <div>Callback</div>;
+
+export default Callback;
--- client/src/hooks/header.js
+++ client/src/hooks/header.js
@@ -1,1 +1,25 @@
 // will contain the links to the components and a login or logout button based on the user authentication state
+import React from "react";
+import { Link } from "react-router-dom";
+import Context from "../utils/context";
+
+const Header = () => {
+  const context = useContext(Context);
+
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/hooksform">Hooks Form</Link>
+      <Link to="/hookscontainer">Hooks Container</Link>
+      <Link to="/privateroute">Private Route</Link>
+      {!context.authState ? (
+        <button onClick={() => context.authObj.login()}>Login</button>
+      ) : (
+        <button onClick={() => context.authObj.logout()}>Logout</button>
+      )}
+    </div>
+  );
+};
+
+export default Header;
